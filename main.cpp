b9a912249ea413a0c723d40115c9c94bbbd650f8
Will Hawkins <whh8b@obs.cr>
2022-05-16 13:40:56 -0400

    Update with some (actual) work!

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@
 #include <llvm-11/llvm/ADT/IntrusiveRefCntPtr.h>
 #include <llvm-11/llvm/ADT/Triple.h>
 #include <llvm-11/llvm/Support/MemoryBuffer.h>
+#include <llvm-11/llvm/Support/VirtualFileSystem.h>
 #include <llvm-11/llvm/Support/raw_ostream.h>
 #include <llvm-11/llvm/Target/TargetOptions.h>
 #include <map>
@@ -60,7 +61,8 @@ class ClangFormatDiagConsumer : public clang::DiagnosticConsumer {
 
 void annotate_location(std::map<unsigned, std::string> &annotations,
                        const clang::SourceManager &sm, clang::Token token,
-                       const std::string &annotation) {
+                       const std::string &start_annotation,
+                       const std::string &end_annotation) {
 
   unsigned begin_offset{0}, end_offset{0};
   begin_offset = sm.getFileOffset(token.getLocation());
@@ -68,9 +70,20 @@ void annotate_location(std::map<unsigned, std::string> &annotations,
   assert(begin_offset != 0);
   assert(end_offset != 0);
 
-  annotations[begin_offset] = "<" + annotation + ">";
-  annotations[end_offset] = "</" + annotation + ">";
+  annotations[begin_offset] = "<" + start_annotation + ">";
+  annotations[end_offset] = "</" + end_annotation + ">";
 
+  // TODO: Determine whether I need to free up the clang variables here.
+  return;
+}
+
+void replace_location(std::map<unsigned, std::string> &replacements,
+                      const clang::SourceManager &sm, clang::Token token,
+                      const std::string &replacement) {
+  unsigned begin_offset{0};
+  begin_offset = sm.getFileOffset(token.getLocation());
+  assert(begin_offset != 0);
+  replacements[begin_offset] = replacement;
   // TODO: Determine whether I need to free up the clang variables here.
   return;
 }
@@ -86,40 +99,33 @@ static clang::FileID create_in_memory_file(
                               clang::SrcMgr::C_User);
 }
 
-clang::tooling::Range
-build_entire_range(const std::unique_ptr<llvm::MemoryBuffer> &Code) {
-  llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> InMemoryFileSystem(
-      new llvm::vfs::InMemoryFileSystem);
-  clang::FileManager Files(clang::FileSystemOptions(), InMemoryFileSystem);
-  clang::DiagnosticsEngine Diagnostics(
-      llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs>(new clang::DiagnosticIDs),
-      new clang::DiagnosticOptions);
-  clang::SourceManager Sources(Diagnostics, Files);
-  clang::FileID ID = create_in_memory_file("<irrelevant>", Code, Sources, Files,
-                                           InMemoryFileSystem);
+clang::tooling::Range build_entire_range(const clang::FileID &fid,
+                                         const clang::SourceManager &sm) {
+  auto Start = sm.getLocForStartOfFile(fid);
+  auto End = sm.getLocForEndOfFile(fid);
 
-  auto Start = Sources.getLocForStartOfFile(ID);
-  auto End = Sources.getLocForEndOfFile(ID);
-
-  return clang::tooling::Range(Sources.getFileOffset(Start),
-                               Sources.getFileOffset(End) -
-                                   Sources.getFileOffset(Start));
+  return clang::tooling::Range(sm.getFileOffset(Start),
+                               sm.getFileOffset(End) - sm.getFileOffset(Start));
 }
 
-std::string clang_format(std::unique_ptr<llvm::MemoryBuffer> Code) {
+std::string clang_format(std::unique_ptr<llvm::MemoryBuffer> Code,
+                         const clang::FileID &fid,
+                         llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> fs,
+                         const clang::FileManager &fm,
+                         clang::SourceManager &sm) {
+
   llvm::Expected<clang::format::FormatStyle> FormatStyle =
-      clang::format::getStyle("LLVM", "input.cpp",
-                              clang::format::DefaultFallbackStyle,
+      clang::format::getStyle("LLVM", "unformatted.cpp", "LLVM",
                               Code->getBuffer(), nullptr);
   assert(!!FormatStyle);
 
-  auto entire_range = build_entire_range(Code);
+  auto entire_range = build_entire_range(fid, sm);
   std::vector<clang::tooling::Range> Ranges{entire_range};
   unsigned CursorPosition{0};
 
   clang::tooling::Replacements cumulative_replacements =
       clang::format::sortIncludes(*FormatStyle, Code->getBuffer(), Ranges,
-                                  "input.cpp", &CursorPosition);
+                                  "unformatted.cpp", &CursorPosition);
 
   auto ChangedCode = clang::tooling::applyAllReplacements(
       Code->getBuffer(), cumulative_replacements);
@@ -129,60 +135,26 @@ std::string clang_format(std::unique_ptr<llvm::MemoryBuffer> Code) {
       cumulative_replacements, Ranges);
   clang::format::FormattingAttemptStatus Status;
   clang::tooling::Replacements format_replacements = clang::format::reformat(
-      *FormatStyle, *ChangedCode, Ranges, "input.cpp", &Status);
+      *FormatStyle, *ChangedCode, Ranges, "unformatted.cpp", &Status);
   cumulative_replacements = cumulative_replacements.merge(format_replacements);
 
-  llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> InMemoryFileSystem(
-      new llvm::vfs::InMemoryFileSystem);
-  clang::FileManager Files(clang::FileSystemOptions(), InMemoryFileSystem);
-
-  llvm::IntrusiveRefCntPtr<clang::DiagnosticOptions> DiagOpts(
-      new clang::DiagnosticOptions());
-  ClangFormatDiagConsumer IgnoreDiagnostics;
-  clang::DiagnosticsEngine Diagnostics(
-      llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs>(new clang::DiagnosticIDs),
-      &*DiagOpts, &IgnoreDiagnostics, false);
-  clang::SourceManager Sources(Diagnostics, Files);
-  clang::FileID ID = create_in_memory_file("input.cpp", Code, Sources, Files,
-                                           InMemoryFileSystem.get());
-  clang::Rewriter rewritere(Sources, clang::LangOptions());
-  clang::tooling::applyAllReplacements(cumulative_replacements, rewritere);
+  clang::Rewriter rewriter(sm, clang::LangOptions());
+  clang::tooling::applyAllReplacements(cumulative_replacements, rewriter);
 
   std::string rewritten_string{};
   llvm::raw_string_ostream rewritten_string_stream{rewritten_string};
-  rewritere.getEditBuffer(ID).write(rewritten_string_stream);
+  rewriter.getEditBuffer(fid).write(rewritten_string_stream);
 
   return rewritten_string;
 }
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    return 1;
-  }
-
-  // First, let's do our clang formatting!
-  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> CodeOrError =
-      llvm::MemoryBuffer::getFileAsStream(argv[1]);
-  assert(!CodeOrError.getError());
-  std::unique_ptr<llvm::MemoryBuffer> Code = std::move(CodeOrError.get());
-  llvm::StringRef CodeStringRef = Code->getBuffer();
-  auto formatted_code_string = clang_format(std::move(Code));
-
-  // Now, let's take that formatted code and treat it is an in-memory file.
-  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> FormattedCodeOrError =
-      llvm::MemoryBuffer::getMemBufferCopy(formatted_code_string);
-  assert(!FormattedCodeOrError.getError());
-  std::unique_ptr<llvm::MemoryBuffer> FormattedCode =
-      std::move(FormattedCodeOrError.get());
-
-  llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> inmemory_fs(
-      new llvm::vfs::InMemoryFileSystem);
-
+void initialize_compilerinstance(
+    clang::CompilerInstance &ci,
+    llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> fs) {
   auto DiagOpts = new clang::DiagnosticOptions();
   clang::TextDiagnosticPrinter *tpd =
       new clang::TextDiagnosticPrinter(llvm::errs(), DiagOpts, false);
   ClangFormatDiagConsumer ignore_diagnostics;
-  clang::CompilerInstance ci{};
 
   std::shared_ptr<clang::TargetOptions> target_options =
       std::make_shared<clang::TargetOptions>();
@@ -191,30 +163,103 @@ int main(int argc, char **argv) {
   auto ti =
       clang::TargetInfo::CreateTargetInfo(ci.getDiagnostics(), target_options);
   ci.setTarget(ti);
-  ci.createFileManager(inmemory_fs);
+  ci.createFileManager(fs);
   ci.createSourceManager(ci.getFileManager());
   ci.createPreprocessor(clang::TU_Complete);
-  clang::FileID ID =
-      create_in_memory_file("input.cpp", FormattedCode, ci.getSourceManager(),
-                            ci.getFileManager(), inmemory_fs);
+}
 
-  clang::Lexer lexer{ID, FormattedCode.get(), ci.getPreprocessor()};
+void print_annotated_file(
+    const std::unique_ptr<llvm::MemoryBuffer> &FormattedCode,
+    std::map<unsigned, std::string> annotations,
+    std::map<unsigned, std::string> replacements) {
+  for (unsigned offset = 0; offset < FormattedCode->getBufferSize(); offset++) {
+    if (annotations.contains(offset)) {
+      std::cout << annotations[offset];
+    }
+    if (replacements.contains(offset)) {
+      std::cout << replacements[offset];
+    } else {
+      std::cout << FormattedCode->getBuffer()[offset];
+    }
+  }
+  std::cout << "\n";
+}
+
+enum class Annotation {
+  Literal,
+  Keyword,
+};
+
+std::map<Annotation, std::pair<std::string, std::string>> styles{
+    {Annotation::Literal, {"font color=red", "font"}},
+    {Annotation::Keyword, {"font color=green", "font"}},
+};
+
+std::map<clang::tok::TokenKind, std::string> replacement_values{
+    {clang::tok::greater, "&gt;"},
+    {clang::tok::less, "&lt;"},
+};
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    return 1;
+  }
+
+  llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> inmemory_fs(
+      new llvm::vfs::InMemoryFileSystem);
+  clang::CompilerInstance ci;
+
+  initialize_compilerinstance(ci, inmemory_fs);
+
+  // First, let's do our clang formatting!
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> CodeOrError =
+      llvm::MemoryBuffer::getFileAsStream(argv[1]);
+  assert(!CodeOrError.getError());
+  std::unique_ptr<llvm::MemoryBuffer> UnformattedCode =
+      std::move(CodeOrError.get());
+  clang::FileID unformatted_fid = create_in_memory_file(
+      "unformatted.cpp", UnformattedCode, ci.getSourceManager(),
+      ci.getFileManager(), inmemory_fs);
+
+  auto formatted_code_string =
+      clang_format(std::move(UnformattedCode), unformatted_fid, inmemory_fs,
+                   ci.getFileManager(), ci.getSourceManager());
+
+  std::cout << formatted_code_string << "\n";
+
+  // Now, let's take that formatted code and treat it is an in-memory file.
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> FormattedCodeOrError =
+      llvm::MemoryBuffer::getMemBufferCopy(formatted_code_string);
+  assert(!FormattedCodeOrError.getError());
+  std::unique_ptr<llvm::MemoryBuffer> FormattedCode =
+      std::move(FormattedCodeOrError.get());
+  clang::FileID formatted_fid = create_in_memory_file(
+      "formatted.cpp", FormattedCode, ci.getSourceManager(),
+      ci.getFileManager(), inmemory_fs);
+
+  clang::Lexer lexer{formatted_fid, FormattedCode.get(), ci.getPreprocessor()};
+
   std::map<unsigned, std::string> annotations{};
+  std::map<unsigned, std::string> replacements{};
 
   clang::SourceLocation location{
-      ci.getSourceManager().getLocForStartOfFile(ID)};
+      ci.getSourceManager().getLocForStartOfFile(formatted_fid)};
+
   while (true) {
     llvm::Optional<clang::Token> token =
         lexer.findNextToken(location, ci.getSourceManager(), ci.getLangOpts());
 
-    if (token->is(clang::tok::eof)) {
+    if (!!token && token->is(clang::tok::eof)) {
       break;
     }
 
-    std::cout << token->getName() << "\n";
-
-    if (token->isLiteral()) {
-      annotate_location(annotations, ci.getSourceManager(), *token, "literal");
+    if (replacement_values.contains(token->getKind())) {
+      replace_location(replacements, ci.getSourceManager(), *token,
+                       replacement_values[token->getKind()]);
+    } else if (token->isLiteral()) {
+      annotate_location(annotations, ci.getSourceManager(), *token,
+                        std::get<0>(styles[Annotation::Literal]),
+                        std::get<1>(styles[Annotation::Literal]));
     } else if (token->is(clang::tok::raw_identifier)) {
       auto idinfo =
           ci.getPreprocessor().getIdentifierInfo(token->getRawIdentifier());
@@ -222,20 +267,17 @@ int main(int argc, char **argv) {
         assert(idinfo);
         if (idinfo->isKeyword(ci.getLangOpts())) {
           annotate_location(annotations, ci.getSourceManager(), *token,
-                            "keyword");
+                            std::get<0>(styles[Annotation::Keyword]),
+                            std::get<1>(styles[Annotation::Keyword]));
         }
       }
     }
     location = token->getEndLoc();
   }
 
-  for (unsigned offset = 0; offset < FormattedCode->getBufferSize(); offset++) {
-    if (annotations.contains(offset)) {
-      std::cout << annotations[offset];
-    }
-    std::cout << FormattedCode->getBuffer()[offset];
-  }
-  std::cout << "\n";
+  std::cout << "<html><head></head><body><pre>\n";
+  print_annotated_file(FormattedCode, annotations, replacements);
+  std::cout << "</pre></body></html>\n";
 
   return 0;
 }
---
+++ testing/testing-formatted.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <junio>
+
+int main() {
+  std::cout << "Testing, one two three?\n";
+  return 0;
+}
